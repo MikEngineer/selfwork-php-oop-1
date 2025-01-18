a60088595ee61a2beb9a9789b75660d0ccc3c174
Michelangelo Ruisi <mruisi96@gmail.com>
2025-01-18 17:24:04 +0100

    selfwork corretto, traccia non chiara

--- index.php
+++ index.php
@@ -7,77 +7,80 @@ class Company{
     public $name;
     public $location;
     public $tot_employees;
-    public $spesaAnnuale;
 
-    // Attributo statico
+    // Attributi Statici
 
-    public static $companies = [];
+    public static $avg_salary = 1500;
+    public static $total = 0;
 
     // Metodi
-    public function __construct($_name, $_location, $_tot_employees = 0, $_spesaAnnuale){
-
+    public function __construct($_name, $_location, $_tot_employees){
         $this->name = $_name;
         $this->location = $_location;
         $this->tot_employees = $_tot_employees;
-        $this->spesaAnnuale = $_spesaAnnuale;
-
-
-        self::$companies[] = $this;
-
     }
 
-    public function dipendenti(){
+    public function checkIfGreater($int1, $int2){
+        if($int1 > $int2){
+            return true;
+        }
+        return false;
+    }
 
-        if($this->tot_employees > 0){
-
+    public function printCheckEmployees($num = 0){
+        if($this->checkIfGreater($this->tot_employees, $num)){
             echo "La società " . $this->name . " con sede in " . $this->location . " ha ben " . $this->tot_employees . " dipendenti.\n";
-
         }else{
-
             echo "La società " . $this->name . " con sede in " . $this->location . " non ha dipendenti.\n";
-
         }
-
     }
 
-    public function spesaAnnuale(){
-
-        echo "La spesa annuale della società " . $this->name . " è di " . $this->spesaAnnuale . " euro.\n";
-
+    public function calculateAnnualCost($int){ //$int sarà 12 come i mesi dell'anno
+        return $this->tot_employees * (self::$avg_salary * $int);
     }
 
-    public static function totaleSpese(){
+    public function printCalculatedAnnualCost($month = 12){
+        echo "-----" . $this->name . "-----\n";
+        echo "Il costo annuale della società " .  $this->name . " è di " . $this->calculateAnnualCost($month) . " euro\n";
+    }
+    
+    public function calculateProgressiveCost($month = 12){
+        return self::$total += $this->calculateAnnualCost($month);
+    }
 
-        $totale = 0;
+    // Metodi statici
 
-        foreach(self::$companies as $company) {
-            $totale += $company->spesaAnnuale;
-        }
-
-        echo "La spesa totale annuale di tutte le società è di " . $totale . " euro.\n";
-
+    public static function printCalculatedFinalTotal(){
+        echo "La Holding ha una previsione di spesa pari a: " . self::$total . "\n";
     }
 
 }
 
-$aulab = new Company('Aulab', 'Italia', 50, 1000);
-$apple = new Company('Apple', 'USA', 137000, 100000000);
-$stopGo = new Company('Stop&Go', 'Italia', 7, 50000);
-$vW = new Company('Volkswagen', 'Germania', 671205, 32548963254);
-$truffa = new Company('Truffa', 'Italia', 0, -45521255);
+$aulab = new Company('Aulab', 'Italia', 50);
+$apple = new Company('Apple', 'USA', 137000);
+$stopGo = new Company('Stop&Go', 'Italia', 7);
+$vW = new Company('Volkswagen', 'Germania', 671205);
+$truffa = new Company('Truffa', 'Italia', 0);
 
-$aulab->dipendenti();
-$apple->dipendenti();
-$stopGo->dipendenti();
-$vW->dipendenti();
-$truffa->dipendenti();
+$aulab->printCheckEmployees();
+$apple->printCheckEmployees();
+$stopGo->printCheckEmployees();
+$vW->printCheckEmployees();
+$truffa->printCheckEmployees();
 
-$aulab->spesaAnnuale();
-$apple->spesaAnnuale();
-$stopGo->spesaAnnuale();
-$vW->spesaAnnuale();
-$truffa->spesaAnnuale();
+$aulab->calculateProgressiveCost();
+$apple->calculateProgressiveCost();
+$stopGo->calculateProgressiveCost();
+$vW->calculateProgressiveCost();
+$truffa->calculateProgressiveCost();
 
-Company::totaleSpese();
+$aulab->printCalculatedAnnualCost();
+$apple->printCalculatedAnnualCost();
+$stopGo->printCalculatedAnnualCost();
+$vW->printCalculatedAnnualCost();
+$truffa->printCalculatedAnnualCost();
+
+
+Company::printCalculatedFinalTotal();
 
 ?>
