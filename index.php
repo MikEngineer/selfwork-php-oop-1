65ee2756f9440232120948546756424769e7d46d
Michelangelo Ruisi <mruisi96@gmail.com>
2025-01-18 18:36:53 +0100

    selfwork ultimato

--- index.php
+++ index.php
@@ -1,86 +1,64 @@
 <?php
 
-// Classe
-class Company{
+class Continent {
+    public $nameContinent;
 
-    // Attributi
-    public $name;
-    public $location;
-    public $tot_employees;
-
-    // Attributi Statici
-
-    public static $avg_salary = 1500;
-    public static $total = 0;
-
-    // Metodi
-    public function __construct($_name, $_location, $_tot_employees){
-        $this->name = $_name;
-        $this->location = $_location;
-        $this->tot_employees = $_tot_employees;
+    public function __construct($continent) {
+        $this->nameContinent = $continent;
     }
-
-    public function checkIfGreater($int1, $int2){
-        if($int1 > $int2){
-            return true;
-        }
-        return false;
-    }
-
-    public function printCheckEmployees($num = 0){
-        if($this->checkIfGreater($this->tot_employees, $num)){
-            echo "La società " . $this->name . " con sede in " . $this->location . " ha ben " . $this->tot_employees . " dipendenti.\n";
-        }else{
-            echo "La società " . $this->name . " con sede in " . $this->location . " non ha dipendenti.\n";
-        }
-    }
-
-    public function calculateAnnualCost($int){ //$int sarà 12 come i mesi dell'anno
-        return $this->tot_employees * (self::$avg_salary * $int);
-    }
-
-    public function printCalculatedAnnualCost($month = 12){
-        echo "-----" . $this->name . "-----\n";
-        echo "Il costo annuale della società " .  $this->name . " è di " . $this->calculateAnnualCost($month) . " euro\n";
-    }
-    
-    public function calculateProgressiveCost($month = 12){
-        return self::$total += $this->calculateAnnualCost($month);
-    }
-
-    // Metodi statici
-
-    public static function printCalculatedFinalTotal(){
-        echo "La Holding ha una previsione di spesa pari a: " . self::$total . "\n";
-    }
-
 }
 
-$aulab = new Company('Aulab', 'Italia', 50);
-$apple = new Company('Apple', 'USA', 137000);
-$stopGo = new Company('Stop&Go', 'Italia', 7);
-$vW = new Company('Volkswagen', 'Germania', 671205);
-$truffa = new Company('Truffa', 'Italia', 0);
+class Country extends Continent {
+    public $nameCountry;
 
-$aulab->printCheckEmployees();
-$apple->printCheckEmployees();
-$stopGo->printCheckEmployees();
-$vW->printCheckEmployees();
-$truffa->printCheckEmployees();
+    public function __construct($continent, $country) {
+        parent::__construct($continent);
+        $this->nameCountry = $country;
+    }
+}
 
-$aulab->calculateProgressiveCost();
-$apple->calculateProgressiveCost();
-$stopGo->calculateProgressiveCost();
-$vW->calculateProgressiveCost();
-$truffa->calculateProgressiveCost();
+class Region extends Country {
+    public $nameRegion;
 
-$aulab->printCalculatedAnnualCost();
-$apple->printCalculatedAnnualCost();
-$stopGo->printCalculatedAnnualCost();
-$vW->printCalculatedAnnualCost();
-$truffa->printCalculatedAnnualCost();
+    public function __construct($continent, $country, $region) {
+        parent::__construct($continent, $country);
+        $this->nameRegion = $region;
+    }
+}
 
+class Province extends Region {
+    public $nameProvince;
 
-Company::printCalculatedFinalTotal();
+    public function __construct($continent, $country, $region, $province) {
+        parent::__construct($continent, $country, $region);
+        $this->nameProvince = $province;
+    }
+}
+
+class City extends Province {
+    public $nameCity;
+
+    public function __construct($continent, $country, $region, $province, $city) {
+        parent::__construct($continent, $country, $region, $province);
+        $this->nameCity = $city;
+    }
+}
+
+class Street extends City {
+    public $nameStreet;
+
+    public function __construct($continent, $country, $region, $province, $city, $street) {
+        parent::__construct($continent, $country, $region, $province, $city);
+        $this->nameStreet = $street;
+    }
+
+    public function getMyCurrentLocation() {
+        echo "Mi trovo in $this->nameContinent, $this->nameCountry, $this->nameRegion, $this->nameProvince, $this->nameCity, $this->nameStreet.";
+    }
+}
+
+$location = new Street('Europa', 'Italia', 'Puglia', 'BA', 'Bari', 'Strada San Giorgio Martire 2D');
+
+$location->getMyCurrentLocation();
 
 ?>
